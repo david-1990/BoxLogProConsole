4b634191834219386d5f65fd6b03f129f096c057
david-1990 <david.duhig90@gmail.com>
2025-11-06 07:22:47 +1000

    Add options menu

--- BoxLogProConsole/Program.cs
+++ BoxLogProConsole/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
+            bool running = true;
 
+            while (running)
+            {
+                Console.WriteLine("BoxLog Pro");
+                Console.WriteLine("1. Add Session");
+                Console.WriteLine("2. View Sessions");
+                Console.WriteLine("3. Exit");
+                Console.Write("Choose an option: ");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        break;
+
+                    case "2":
+                        break;
+
+                    case "3":
+                        running = false;
+                        Console.WriteLine("Goodbye!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option.\n");
+                        break;
+                }
+            }
         }
     }
 }
