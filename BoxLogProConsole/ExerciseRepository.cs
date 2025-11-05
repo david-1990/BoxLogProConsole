4602fb6c7b9b1f979bdaf477393049debfd214de
david-1990 <david.duhig90@gmail.com>
2025-11-06 08:15:00 +1000

    Create exercise and exercise repo classes

---
+++ BoxLogProConsole/ExerciseRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxLogProConsole
+{
+    public static class ExerciseRepository
+    {
+        public static List<Exercise> GetAvailableExercises()
+        {
+            return new List<Exercise>
+        {
+            new Exercise { Id = 1, Name = "Shadow Boxing" },
+            new Exercise { Id = 2, Name = "Heavy Bag" },
+            new Exercise { Id = 3, Name = "Footwork Drills" },
+            new Exercise { Id = 4, Name = "Sparring" },
+            new Exercise { Id = 5, Name = "Speed Ladder" }
+        };
+        }
+    }
+}
